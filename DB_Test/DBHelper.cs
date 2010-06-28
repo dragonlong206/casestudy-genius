04c8f596ef754c157f3ae2de78b7a637c69ce1e7
lv.long.tn <lv.long.tn@9ef3af87-62f7-3b32-f918-ade29adfb4d5>
2010-06-28 13:59:21 +0000

    Bổ sung các tài liệu liên quan đến DB test

---
+++ DB_Test/DBHelper.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Data;
+using System.Configuration;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Data.SqlClient;
+
+namespace AdvisingSystem.Lib
+{
+    // Exception for database connection error
+    public class DBHelperException : Exception
+    {
+        private string _message;
+        public DBHelperException(string message)
+        {
+            _message = message;
+        }
+
+        public string Message
+        {
+            get { return _message; }
+        }
+    }
+
+    // Database helper class for reading and writing data to database
+    public class DBHelper
+    {
+        // Singleton: keep unique instance of database connection throughout application
+        static DBHelper _dbHelper = null;
+        private DBHelper()
+        {
+        }
+
+        // sql connection member
+        SqlConnection _conn;
+
+        // Reading and writing methods
+        public Object ExecuteScalar(SqlCommand command)
+        {
+            try
+            {
+                command.Connection = _conn;
+                return command.ExecuteScalar();
+            }
+
+            catch (Exception currentException)
+            {
+                throw new DBHelperException(currentException.Message);
+            }
+        }
+
+        public SqlDataReader ExecuteReader(SqlCommand command)
+        {
+            try
+            {
+                command.Connection = _conn;
+                return command.ExecuteReader();
+            }
+
+            catch (Exception currentException)
+            {
+                throw new DBHelperException(currentException.Message);
+            }
+        }
+
+        public int ExecuteNonQuery(SqlCommand command)
+        {
+            try
+            {
+                command.Connection = _conn;
+                return command.ExecuteNonQuery();
+            }
+
+            catch (Exception currentException)
+            {
+                throw new DBHelperException(currentException.Message);
+            }
+        }
+
+        public DataTable RetrieveTable(SqlCommand command)
+        {
+            try
+            {
+                command.Connection = _conn;
+                SqlDataAdapter adapter = new SqlDataAdapter(command);
+                DataTable table = new DataTable();
+                adapter.Fill(table);
+                return table;
+            }
+
+            catch (Exception currentException)
+            {
+                throw new DBHelperException(currentException.Message);
+            }
+        }
+
+        public DataSet RetrieveDataSet(SqlCommand command)
+        {
+            try
+            {
+                command.Connection = _conn;
+                SqlDataAdapter adapter = new SqlDataAdapter(command);
+                DataSet dataSet = new DataSet();
+                adapter.Fill(dataSet);
+                return dataSet;
+            }
+
+            catch (Exception currentException)
+            {
+                throw new DBHelperException(currentException.Message);
+            }
+        }
+
+        // Opening and closing methods
+        public void Open()
+        {
+            try
+            {
+                if(_conn.State == ConnectionState.Closed)
+                    _conn.Open();
+
+            }
+
+            catch (Exception currentException)
+            {
+                throw new DBHelperException(currentException.Message);
+            }
+        }
+
+        public void Close()
+        {
+            _conn.Close();
+        }
+
+        // static method to retrieve unique instance of database connection
+        public static DBHelper Connection
+        {
+            get
+            {
+                try
+                {
+                    if (_dbHelper == null)
+                    {
+                        _dbHelper = new DBHelper();
+                        _dbHelper._conn = new SqlConnection(ConfigurationManager.ConnectionStrings["primaryConnection"].ToString());
+                        _dbHelper._conn.Close();
+                    }
+                    else
+                    {
+                        if (_dbHelper._conn.State == ConnectionState.Open)
+                            _dbHelper._conn.Close();
+                    }
+                    return _dbHelper;
+                }
+
+                catch (Exception currentExeption)
+                {
+                    throw new DBHelperException(currentExeption.Message);
+                }
+            }
+        }
+
+        public static void SetConnection(string connection)
+        {
+            if (_dbHelper == null)
+            {
+                _dbHelper = new DBHelper();
+            }
+            _dbHelper._conn = new SqlConnection(connection);
+        }
+    }
+}
---
+++ DB_Test/UnitTest/UserModelTest.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using NUnit.Mocks;
+using AdvisingSystem.Lib.Model;
+using AdvisingSystem.Lib.Entity;
+using System.Data.SqlClient;
+
+namespace UnitTest
+{
+    [TestFixture]
+    public class UserModelTest
+    {
+        UserModel userModel;
+        
+        [SetUp]
+        public void InitializeTest()
+        {
+            userModel = new UserModel();
+            AdvisingSystem.Lib.DBHelper.SetConnection("Data Source=LEVANLONG;Initial Catalog=AdvisingSystem;Integrated Security=True");
+        }
+
+        [Test]
+        public void GetUser_UserName_Password()
+        {
+            //Role role = new Role(1, "Sinh viên");
+            //User expected = new User("0712255", "Lê Văn Long", "15/09/1989", 0, "lv.long.tn@gmail.com", role);
+
+            User real = userModel.GetUser("0712255", "123456");
+            Assert.That(real.DOB, Is.EqualTo(new DateTime(1989, 9, 15)));
+            Assert.That(real.Email, Is.EqualTo("lv.long.tn@gmail.com"));
+            Assert.That(real.Name, Is.EqualTo("Lê Văn Long"));
+            Assert.That(real.Role.ID, Is.EqualTo(1));
+            Assert.That(real.Sex, Is.EqualTo(0));
+            Assert.That(real.Username, Is.EqualTo("0712255"));
+        }
+
+        [Test]
+        public void GetUser_NotExist()
+        {
+            User real1 = userModel.GetUser("000000", "123456");
+            Assert.That(real1, Is.EqualTo(null));
+        }
+
+        [Test]
+        public void GetUser_ExistButWrongPassword()
+        {
+            User real = userModel.GetUser("0712255", "123457");
+            Assert.That(real, Is.EqualTo(null));
+        }
+
+        [Test]
+        public void GetStudent_NotExist()
+        {
+            Student real = userModel.GetStudent("000000");
+            Assert.That(real, Is.EqualTo(null));
+        }
+
+        [Test]
+        public void GetStudent_UserName()
+        {
+            Student real = userModel.GetStudent("0712255");
+
+            Assert.That(real.DOB, Is.EqualTo(new DateTime(1989, 9, 15)));
+            Assert.That(real.Email, Is.EqualTo("lv.long.tn@gmail.com"));
+            Assert.That(real.Name, Is.EqualTo("Lê Văn Long"));
+            Assert.That(real.Role.ID, Is.EqualTo(1));
+            Assert.That(real.Sex, Is.EqualTo(0));
+            Assert.That(real.Username, Is.EqualTo("0712255"));
+            Assert.That(real.CourseYear, Is.EqualTo(2007));
+            Assert.That(real.Curriculum.ID, Is.EqualTo(2));
+            Assert.That(real.GPA, Is.EqualTo(null));
+            Assert.That(real.Major, Is.EqualTo(null));
+        }
+
+        [Test]
+        public void GetStudent_Exist_NotStudent()
+        {
+            Student real = userModel.GetStudent("lqvu");
+            Assert.That(real, Is.EqualTo(null));
+        }
+
+        [Test]
+        public void GetUser_UserName()
+        {
+            User real = userModel.GetUser("0712255");
+            Assert.That(real.DOB, Is.EqualTo(new DateTime(1989, 9, 15)));
+            Assert.That(real.Email, Is.EqualTo("lv.long.tn@gmail.com"));
+            Assert.That(real.Name, Is.EqualTo("Lê Văn Long"));
+            Assert.That(real.Role.ID, Is.EqualTo(1));
+            Assert.That(real.Sex, Is.EqualTo(0));
+            Assert.That(real.Username, Is.EqualTo("0712255"));
+        }
+
+        [Test]
+        public void GetUser_UserName_NotExist()
+        {
+            User real = userModel.GetUser("000000");
+            Assert.That(real, Is.EqualTo(null));
+        }
+
+        [Test]
+        public void GetUsers_RoleID_Exist()
+        {
+            List<User> real = userModel.GetUsers(1);
+            foreach (User user in real)
+            {
+                Assert.That(user.Role.ID, Is.EqualTo(1));
+            }
+        }
+
+        [Test]
+        public void GetUsers_RoleID_NotExist()
+        {
+            List<User> real = userModel.GetUsers(5);
+            Assert.That(real.Count, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void GetRoles()
+        {
+            List<Role> expected = new List<Role>();
+            expected.Add(new Role(1, "Sinh viên"));
+            expected.Add(new Role(2, "Quản lý"));
+            expected.Add(new Role(3, "Giáo viên"));
+
+            List<Role> real = userModel.GetRoles();
+            //CollectionAssert.AreEqual(expected, real);
+            //CollectionAssert.AreEquivalent(expected, real);
+
+            Assert.That(real[0].ID, Is.EqualTo(1));
+            Assert.That(real[0].Name, Is.EqualTo("Sinh viên"));
+
+            Assert.That(real[1].ID, Is.EqualTo(2));
+            Assert.That(real[1].Name, Is.EqualTo("Quản lý"));
+
+            Assert.That(real[2].ID, Is.EqualTo(3));
+            Assert.That(real[2].Name, Is.EqualTo("Giáo viên"));
+        }
+    }
+}
